d7980071dd25713d8097e4d975ee799465c6b741
ahmed-ba <58521989+ahmed-ba@users.noreply.github.com>
2020-05-20 23:03:59 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-   apiKey: "AIzaSyD_8WV3aVnxAtTIZ8MAAnKWiEe9qgznCas",
+  apiKey: "AIzaSyD_8WV3aVnxAtTIZ8MAAnKWiEe9qgznCas",
   authDomain: "crud-firebase-49b77.firebaseapp.com",
   databaseURL: "https://crud-firebase-49b77.firebaseio.com",
   projectId: "crud-firebase-49b77",
   storageBucket: "crud-firebase-49b77.appspot.com",
   messagingSenderId: "133356179306",
   appId: "1:133356179306:web:f3745f0283c4fc19065f13",
-  measurementId: "G-VKHXVXS9LJ"
+  measurementId: "G-VKHXVXS9LJ"   
     }
 };
 
